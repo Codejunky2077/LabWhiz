864b11173b88d1e5cda2a9dd0b0c7dbc76859f96
Aniket <aniketbhilai2042@gmail.com>
2025-07-20 12:13:03 +0530

    changed raw link

--- LabWhiz.py
+++ LabWhiz.py
@@ -10,7 +10,7 @@ from sidebar import side_barfunc
 #adding download 
 # Inject the manifest and meta tags
 components.html("""
-                <link rel="manifest" href="https://raw.githubusercontent.com/Codejunky2077/LabWhiz/refs/heads/main/manifest.json">
+                <link rel="manifest" href="https://raw.githubusercontent.com/Codejunky2077/LabWhiz/main/manifest.json">
                 <meta name="theme-color" content="#1f2937"/>
                 <link rel="icon" href="https://raw.githubusercontent.com/Codejunky2077/LabWhiz/main/lab-items-192.png" type="image/png">
                 """, height=0)
