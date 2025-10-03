0f8cb535c16051bb8a76fb0c826c038081490168
Aniket <aniketbhilai2042@gmail.com>
2025-10-04 00:10:45 +0530

    finalising qr

--- sidebar.py
+++ sidebar.py
@@ -9,7 +9,7 @@ def side_barfunc():
         
         with st.sidebar.expander("📑 About us:",expanded=True):
             st.markdown("""
-                    - <a href="https://labwhiz.netlify.app/" target="_blank">LabWhiz blog 🖊️</a><br>
+                    - <a href="https://labwhiz.netlify.app/" target="_blank">LabWhiz 🖊️</a><br>
                         """,unsafe_allow_html=True)
 
         with st.sidebar.expander("➗ Recent Calculations", expanded=False):
@@ -22,10 +22,11 @@ def side_barfunc():
         with st.sidebar.expander("🕊️ Share LabWhiz", expanded=False):
             st.markdown("""
                     <br><small>Send it to your labmates:</small><br>
+                    - <a href="https://github.com/Codejunky2077/LabWhiz/blob/main/LabWhiz_QR.png?raw=true" target="_blank">QR code</a><br>
                     - <a href="https://api.whatsapp.com/send?text=LabWhiz%20%E2%80%94%20Built%20for%20life%20science%20lab%20people%20who%20hate%20messy%20calculations.%0A%0ANo%20more%20unit%20confusion%20or%20endless%20Googling.%0A%F0%9F%93%B1%20Works%20on%20mobile.%20No%20login.%20No%20ads.%0A%F0%9F%92%A1%20Many%20are%20already%20using%20it.%0A%0A%F0%9F%91%87%20Try%20it%20once%20%E2%80%94%20you%E2%80%99ll%20wish%20you%20had%20it%20earlier%3A%0Ahttps%3A%2F%2Flabwhiz.streamlit.app%2F" target="_blank">WhatsApp</a><br>
-                    - <a href="https://twitter.com/intent/tweet?text=Try%20LabWhiz%20%E2%80%94%20The%20fastest%20lab%20calculator.%20https://labwhiz.streamlit.app/" target="_blank">🕊️X</a><br>
-                    - <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://labwhiz.streamlit.app/" target="_blank">💼LinkedIn</a><br>
-                    - <a href="mailto:?subject=Try%20LabWhiz%20%E2%80%94%20Fastest%20Lab%20Calculator&body=LabWhiz%20is%20a%20mobile-friendly%20lab%20calculator%20built%20for%20life%20science%20students.%0A%0Ahttps://labwhiz.streamlit.app/" target="_blank">📨 Email</a>
+                    - <a href="https://twitter.com/intent/tweet?text=Try%20LabWhiz%20%E2%80%94%20The%20fastest%20lab%20calculator.%20https://labwhiz.streamlit.app/" target="_blank"> X </a><br>
+                    - <a href="https://www.facebook.com/sharer/sharer.php?u=https://labwhiz.streamlit.app/" target="_blank">Facebook</a><br>
+                    - <a href="mailto:?subject=Try%20LabWhiz%20%E2%80%94%20Fastest%20Lab%20Calculator&body=LabWhiz%20is%20a%20mobile-friendly%20lab%20calculator%20built%20for%20life%20science%20students.%0A%0Ahttps://labwhiz.streamlit.app/" target="_blank"> Email</a>
                     """, unsafe_allow_html=True)
                             
         with st.sidebar.expander("💬 Want to make LabWhiz better? share your suggestions",expanded=False):
